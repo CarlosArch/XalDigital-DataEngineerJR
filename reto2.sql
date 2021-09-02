1ad0e90eb8217e942e36b623a1476ac66153f3f6
CarlosArch <carlos_arch@hotmail.com>
2021-09-02 13:05:14 -0500

    reto 2: carga de datos en tablas

--- reto2.sql
+++ reto2.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS aerolineas;
+DROP TABLE IF EXISTS aeropuertos;
+DROP TABLE IF EXISTS movimientos;
+DROP TABLE IF EXISTS vuelos;
+
 CREATE TABLE aerolineas (
     id_aerolinea     INT  PRIMARY KEY
    ,nombre_aerolinea TEXT
@@ -18,7 +23,52 @@ CREATE TABLE vuelos (
    ,id_aeropuerto INT
    ,id_movimiento INT
    ,dia           TEXT
-   FOREIGN KEY(id_aerolinea) REFERENCES aerolineas (id_aerolinea)
-   FOREIGN KEY(id_aeropuerto) REFERENCES aeropuertos (id_aeropuerto)
-   FOREIGN KEY(id_movimiento) REFERENCES movimientos (id_movimiento)
+   ,FOREIGN KEY(id_aerolinea) REFERENCES aerolineas (id_aerolinea)
+   ,FOREIGN KEY(id_aeropuerto) REFERENCES aeropuertos (id_aeropuerto)
+   ,FOREIGN KEY(id_movimiento) REFERENCES movimientos (id_movimiento)
 );
+
+INSERT INTO aerolineas (
+    id_aerolinea
+   ,nombre_aerolinea
+) VALUES
+    (1 ,'Volaris'   )
+   ,(2 ,'Aeromar'   )
+   ,(3 ,'Interjet'  )
+   ,(4 ,'Aeromexico')
+;
+
+INSERT INTO aeropuertos (
+    id_aeropuerto
+   ,nombre_aerolinea
+) VALUES
+    (1 ,'Benito Juarez')
+   ,(2 ,'Guanajuato'   )
+   ,(3 ,'La paz'       )
+   ,(4 ,'Oaxaca'       )
+;
+
+INSERT INTO movimientos (
+    id_movimiento
+   ,descripcion
+) VALUES
+    (1 ,'Salida' )
+   ,(2 ,'Llegada')
+;
+
+INSERT INTO vuelos (
+    id_aerolinea
+   ,id_aeropuerto
+   ,id_movimiento
+   ,dia
+) VALUES
+    (1 ,1 ,1 ,'2021-05-02')
+   ,(2 ,1 ,1 ,'2021-05-02')
+   ,(3 ,2 ,2 ,'2021-05-02')
+   ,(4 ,3 ,2 ,'2021-05-02')
+   ,(1 ,3 ,2 ,'2021-05-02')
+   ,(2 ,1 ,1 ,'2021-05-02')
+   ,(2 ,3 ,1 ,'2021-05-04')
+   ,(3 ,4 ,1 ,'2021-05-04')
+   ,(3 ,4 ,1 ,'2021-05-04')
+;
